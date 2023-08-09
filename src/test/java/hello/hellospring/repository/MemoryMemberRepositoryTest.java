92050cb6f7b3108e313a8e0a332ea927760b1de5
조승빈 <joseungbin@joseungbin-ui-MacBookAir.local>
2023-08-09 17:07:08 +0900

    first commit

---
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,58 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("string");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("string1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("string2");
+        repository.save(member2);
+
+        Member result = repository.findByName("string1").get();
+
+        Assertions.assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("string1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("string2");
+        repository.save(member2);
+
+        //when
+        List<Member> result = repository.findAll();
+
+    }
+}
